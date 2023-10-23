b9716b7f3607c38b7f3b3a0948c0fb1eb92097e6
Ana <Ana@DESKTOP-6E2VQOM>
2023-10-23 22:28:35 +0300

    Add project files.

---
+++ Data/Sighiartau_Adriana_Lab2Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Sighiartau_Adriana_Lab2.Models;
+
+namespace Sighiartau_Adriana_Lab2.Data
+{
+    public class Sighiartau_Adriana_Lab2Context : DbContext
+    {
+        public Sighiartau_Adriana_Lab2Context (DbContextOptions<Sighiartau_Adriana_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Sighiartau_Adriana_Lab2.Models.Book> Book { get; set; } = default!;
+    }
+}
